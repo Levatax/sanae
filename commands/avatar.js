40e537159bc40ff9384b150a63218f5977b9026a
Levatax <godlypain@animsy.com>
2020-09-22 07:21:53 +0000

    Added avatar command

---
+++ commands/avatar.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+exports.run = async(bot, message, args) => {
+let user = message.mentions.users.first() || message.guild.member(args[0]) || message.author;
+message.channel.sendEmbed(new Discord.RichEmbed()
+.setDescription('Here:')
+.setImage(user.avatarURL)
+.setColor("RANDOM"));
+}
+
+module.exports.help = {
+    name: 'pfp',
+    aliases: ['avatar', 'pp']
+};
