786d4789037b4057e9cd8c0c3511517dbefabcbf
Levatax <godlypain@animsy.com>
2020-09-24 07:13:10 +0000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 
 exports.run = async(bot, message, args) => {
 let user = message.mentions.users.first() || message.guild.member(args[0]) || message.author;
-message.channel.sendEmbed(new Discord.RichEmbed()
+message.channel.send(new Discord.RichEmbed()
 .setDescription('Here:')
 .setImage(user.avatarURL)
 .setColor("RANDOM"));
