69c79d977c4816f3a97a58041a6d0ecc6c9ef9c5
Levatax <godlypain@animsy.com>
2020-09-24 08:00:56 +0000

    Add new file

---
+++ commands/bulkdelete.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+exports.run = async(bot, message, args) => {
+    let number = parseInt(args.join(' '));
+    if (!number) return message.channel.send('Please enter number and try again');
+    if (number > 99) return message.channel.send(`I can't delete more than 100 message`);
+    message.channel.bulkDelete(number + 1);
+    message.channel.send('Cleared ' + number + ' message')
+};
+
+module.exports.help = {
+    name: 'clear',
+    aliases: ['delete']
+};
